7150e6671c33947e3ab1867f19874e4cd1eaf9c1
Barcom <103148113+Barc0m@users.noreply.github.com>
2022-06-27 21:42:32 -0300

    Add files via upload

---
+++ Primeiro PHP.php
@@ -0,0 +1,16 @@
+<html lang="pt-br">
+    <head>
+        <title>Primeiro PHP</title>
+    </head>
+
+    <body>
+
+        <?php
+            for ( &i = 0 ; $i < 10 ; $1++ ) {
+                print( "Linha número " . $1 . "<br />")
+            }
+        ?>
+
+    </body>
+
+</html>
